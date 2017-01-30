be900e5e5ead68f21520d31b086114c32d21c3d6
RoastedTurkey <adriaanvanoostveen@gmail.com>
2017-01-30 12:24:34 +0100

    Update ITTT_test3.ino

--- Code Examples/Final Code/ITTT_test3.ino
+++ Code Examples/Final Code/ITTT_test3.ino
@@ -7,22 +7,40 @@
 // LED array
 CRGB leds[NUM_LEDS];
 
+//Flames data
+CRGB fire[18];
+int fireFrame = 0;
+int fireWait = 0;
+int lavaStart = 18;
+int lavaLength = 5;
+
+boolean dash = false;
+boolean gameOver = false;
+boolean nextLevel = false;
+
 void setup() {
   Serial.begin(9600);
 
   // Stick up
-  pinMode(2, INPUT);
+  pinMode(2, INPUT_PULLUP);
   // Stick down
-  pinMode(4, INPUT);
-  // Button
-  pinMode(7, INPUT);
-  // Flex resistor
-  pinMode(A0, INPUT);
-  // LDR
-  pinMode(A1, INPUT);
+  pinMode(4, INPUT_PULLUP);
+//  // Button
+//  pinMode(7, INPUT_PULLUP);
+//  // Flex resistor
+//  pinMode(A0, INPUT_PULLUP);
+//  // LDR
+//  pinMode(A1, INPUT_PULLUP);
   // LED arrangement
-  FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
+  //putting frames in the flames
+  for(int i=0; i<12;i++){
+    fire[i] = CRGB(14*i,5*i,0);
+  }
 }
+  int pcPos = 10;
+  int pcSpeed = 1;
+  int deadTimer = 0;
 
 void loop() {
 //  leds[0] = CRGB(255,0,0);
@@ -36,54 +54,101 @@ void loop() {
   int stickUp = digitalRead(2);
   int stickDown = digitalRead(4);
   int buttonDown = digitalRead(7);
-  int pcPos = 0;
-  int pcSpeed = 1;
-  boolean dash = false;
-  boolean gameOver = false;
+  
+//  Serial.print("up = ");
+//  Serial.print(stickUp);
+//  Serial.print(", down = ");
+//  Serial.println(stickDown);
+  Serial.print("pcPos = ");
+  Serial.print(pcPos);
+  Serial.print(", fire = ");
+  Serial.print(fireFrame);
+  Serial.print(", no fire= ");
+  Serial.print(fireWait);
+  Serial.print(", gameover = ");
+  Serial.print(gameOver);
+  Serial.print(", d.count = ");
+  Serial.println(deadTimer);
   
   //Stick changing the player position
-  if (dash == false) {
-    if (stickUp == 1) {
-      if (pcPos > NUM_LEDS)  {
-        pcPos = NUM_LEDS - 1;
-      }
-      else{
-        pcPos = pcPos + pcSpeed;
+  if (dash == false && gameOver == false && nextLevel == false) {
+    if (stickUp == 0) {
+//      pcPos = pcPos + pcSpeed;
+      pcPos++;
+        if (pcPos > NUM_LEDS - 1) {
+          pcPos = NUM_LEDS - 1;
+          nextLevel = true;
       }
     }
-    else if (stickDown == 1) {
+    else if (stickDown == 0) {
+//      pcPos = pcPos - pcSpeed;
+      pcPos = pcPos - 1;
       if (pcPos <0)  {
         pcPos = 0;
       }
-      else{
-        pcPos = pcPos - pcSpeed;
-      }
     }
   }
 
-  //Button + stick in a direction to dash WIP
-  if (dash == false) {
-    if (buttonDown == 1 && stickUp == 1) {
-      dash = true;
-      // some dashing code
-      dash = false;
-    }
-    else if (buttonDown == 1 && stickDown == 1) {
-      dash = true;
-      // some dashing code
-      dash = false;
-    }
-  }
+//  //Button + stick in a direction to dash WIP
+//  if (dash == false) {
+//    if (buttonDown == 1 && stickUp == 1) {
+//      dash = true;
+//      // some dashing code
+//      dash = false;
+//    }
+//    else if (buttonDown == 1 && stickDown == 1) {
+//      dash = true;
+//      // some dashing code
+//      dash = false;
+//    }
+//  }
 
   //lava floor
-  int lavaStart = 18;
-  int lavaLength = 5;
-  if (pcPos >= lavaStart && pcPos <= lavaStart + lavaLength) {
+  if (pcPos >= lavaStart && pcPos < lavaStart + lavaLength && fireFrame > 8 && fireWait == 17) {
     gameOver = true;
   }
+  
   // refreshing the "screen"
   for(int i = 0;i< NUM_LEDS;i++){
     leds[i] = CRGB(0,0,0);
   }
+   
+  if(fireWait >16){
+    for(int j = 0;j<lavaLength;j++){
+      leds[lavaStart + j] = fire[fireFrame];
+    }
+    fireFrame++;
+    if(fireFrame >16){
+      fireWait = 0;
+    }
+  }
+  else {
+    fireWait++;
+    if(fireWait >16){
+      fireFrame = 0;
+    }
+  }
+  
   leds[pcPos] = CRGB(0,255,0);
-}
+  
+  if(gameOver == true || nextLevel == true){
+    for(int k = 0; k<NUM_LEDS; k++){
+      if(gameOver == true){
+        leds[k] = CRGB(255,0,0);
+      }
+      else{
+        leds[k] =  CRGB(0,255,0);
+      }
+    }
+    deadTimer++;
+    if (deadTimer >22){
+      pcPos = 1;
+      gameOver = false;
+      nextLevel = false;
+      deadTimer = 0;
+      lavaStart = random(6,76);
+    }
+  }
+  FastLED.show();
+//  delay(200);
+  }
